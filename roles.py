0bcb52f55d4edd37c64110a7d5365c5b6eda8f5f
Eric <fingereric@web.de>
2022-08-14 19:27:07 +0200

    error handling for reaction.remove

--- roles.py
+++ roles.py
@@ -110,7 +110,12 @@ async def restore_reaction_roles(bot):
                     # Check if the User is a Member:
                     if _guild.get_member(member.id) is None:
                         logger.info(f"User '{member.name}' is not a Member. Removing his reaction")
-                        await reaction.remove(member)
+                        try:
+                            await reaction.remove(member)
+                        except discord.HTTPException:
+                            logger.error("HTTPException")
+                        except Exception as e:
+                            logger.error(e)
                         continue
 
                     # Skip admins bc we don't want to get EVERY role all the time
